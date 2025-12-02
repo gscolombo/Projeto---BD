0c618273767c5247fa882d1e82f3e5c4d7c05e60
Gabriel de Souza Colombo <gscolombo404@gmail.com>
2025-12-02 09:20:27 -0300

    feat: script Bash para inicialização de banco de dados

---
+++ sql/init.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+read -p "Insira o nome do banco de dados: " db
+read -p "Insira o nome de usuário: " username
+
+psql -d $db -U $username -c "\i sql/drop_db.sql"
+psql -d $db -U $username -c "\i sql/create_db.sql"
+psql -d $db -U $username -c "\i sql/populate_db.sql"
+
+
+base_path=$(dirname "${BASH_SOURCE[0]}")
+
+views_dir="$base_path/views"
+procedures_dir="$base_path/procedures"
+
+if [[ -d $views_dir ]]; then
+    for file in "$views_dir"/*; do
+        if [[ -f $file ]] && [[ $file == *.sql ]]; then 
+            psql -d $db -U $username -c "\i $file"
+        fi
+    done
+fi
+
+if [[ -d $procedures_dir ]]; then
+    for file in "$procedures_dir"/*; do
+        if [[ -f $file ]] && [[ $file == *.sql ]]; then 
+            psql -d $db -U $username -c "\i $file"
+        fi
+    done
+fi
