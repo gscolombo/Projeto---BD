2775e051e88112590cbafb2424d02efe0e7bde5c
Gabriel de Souza Colombo <gscolombo404@gmail.com>
2025-12-04 11:19:21 -0300

    fix: ajuste de view

--- sql/views/local_overview_view.sql
+++ sql/views/local_overview_view.sql
@@ -1,13 +1,12 @@
 CREATE OR REPLACE VIEW local_overview AS
 SELECT 
-    l.id_local,
     l.nome,
     l.lat,
-    l.long,
+    l.lng,
     a.nome_arquivo,
     a.tipo,
     a.tamanho,
     a.conteudo  -- BYTEA
 FROM local l
 LEFT JOIN local_arquivo a
-       ON l.id_local = a.id_local;
+       ON (l.lat, l.lng) = (a.local_lat, a.local_lng);
