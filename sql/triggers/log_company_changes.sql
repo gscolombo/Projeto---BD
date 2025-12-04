df75efeeebc9d307ea45824618ca68d3b40b67e1
eocarv <eduardodepc@gmail.com>
2025-12-03 22:51:09 -0300

    fix: correção nas views e triggers

--- sql/triggers/log_company_changes.sql
+++ sql/triggers/log_company_changes.sql
@@ -1,55 +1,44 @@
-CREATE TABLE IF NOT EXISTS company_log (
-    id_log SERIAL PRIMARY KEY,
-    id_company INT,
-    changed_at TIMESTAMP DEFAULT NOW(),
-    operation TEXT,
-    old_data JSONB,
-    new_data JSONB
-);
-
--- Funcao que registra mudanças na tabela "company"
-CREATE OR REPLACE FUNCTION log_company_changes_func()
+CREATE OR REPLACE FUNCTION log_empresa_changes_func()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Caso seja atualizacao
     IF TG_OP = 'UPDATE' THEN
-        INSERT INTO company_log(id_company, operation, old_data, new_data)
+        INSERT INTO empresa_log(id_empresa, operation, old_data, new_data)
         VALUES (
-            OLD.id_company,
+            OLD.cnpj,
             'UPDATE',
             to_jsonb(OLD),
             to_jsonb(NEW)
         );
-
+        RETURN NEW;
     -- Caso seja insercao
     ELSIF TG_OP = 'INSERT' THEN
-        INSERT INTO company_log(id_company, operation, new_data)
+        INSERT INTO empresa_log(id_empresa, operation, new_data)
         VALUES (
-            NEW.id_company,
+            NEW.cnpj,
             'INSERT',
             to_jsonb(NEW)
         );
-
+        RETURN NEW;
     -- Caso seja exclusao
     ELSIF TG_OP = 'DELETE' THEN
-        INSERT INTO company_log(id_company, operation, old_data)
+        INSERT INTO empresa_log(id_empresa, operation, old_data)
         VALUES (
-            OLD.id_company,
+            OLD.cnpj,
             'DELETE',
             to_jsonb(OLD)
         );
+        RETURN OLD;
     END IF;
 
-    RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
 
+-- Trigger que chama a funcao apos qualquer operacao em "empresa"
+DROP TRIGGER IF EXISTS empresa_changes_trigger ON empresa;
 
--- Trigger que chama a funcao apos qualquer operacao em "company"
-DROP TRIGGER IF EXISTS company_changes_trigger ON company;
-
-CREATE TRIGGER company_changes_trigger
+CREATE TRIGGER empresa_changes_trigger
 AFTER INSERT OR UPDATE OR DELETE
-ON company
+ON empresa
 FOR EACH ROW
-EXECUTE PROCEDURE log_company_changes_func();
+EXECUTE PROCEDURE log_empresa_changes_func();
